c26bcc8ea06ecb3a80351cf00a5e8b8989b6ea37
Azure Wolf <trngtrunghieu70g1@gmail.com>
2026-09-05 13:20:38 +0000

    Cập nhật giao diện profile AzureWolf

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.PROFILE_CONFIG = {
   minecraftServerIp: "furrymc.fun",
   discordUserId: "1315287238120046596",
-  counterApiUrl: "",
+  counterApiUrl: "https://pay.furrymc.fun/api/profile-view",
 
   // Điền thông tin dưới đây để tự bật mã QR trong mục "Nuôi tôi".
   // bankCode ví dụ: BIDV, MBBank, VCB, ACB, TCB...
