34a824863930d5c51823f4449be72031708d8b05
Azure Wolf <trngtrunghieu70g1@gmail.com>
2026-09-05 12:47:16 +0000

    Cập nhật giao diện profile AzureWolf

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ window.PROFILE_CONFIG = {
 
   // Điền thông tin dưới đây để tự bật mã QR trong mục "Nuôi tôi".
   // bankCode ví dụ: BIDV, MBBank, VCB, ACB, TCB...
-  bankCode: "",
-  bankAccount: "",
-  bankAccountName: "",
+  bankCode: "BIDV",
+  bankAccount: "8877495219",
+  bankAccountName: "TRAN NGUYEN TRUNG HIEU",
   donationNote: "NUOIAZUREWOLF"
 };
